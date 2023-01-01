a3aa2f41564c26c3dbf9a5b9a05e44f121917d37
Camilo Agudelo <agudelogarciacamilo@gmail.com>
2023-01-01 05:34:41 +0100

    Feature: Creación de los módulos requeridos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MostradorUsuariosModule } from './mostrador-usuarios/mostrador-usuarios.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    MostradorUsuariosModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
