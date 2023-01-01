cf3322f5448c3baaa230e9a4aed2bbbb00c0d955
Camilo Agudelo <agudelogarciacamilo@gmail.com>
2023-01-01 19:21:07 +0100

    Feaure: Creación de los modelos/interfaces principales del proyecto.

---
+++ src/app/models/github-repo-dto.interface.ts
@@ -0,0 +1,32 @@
+import { GitHubUserDTO } from "./github-user-dto.interface";
+
+export interface GitHubReposDTO {
+  id:                          number;
+  node_id:                     string;
+  name:                        string;
+  full_name:                   string;
+  private:                     boolean;
+  owner:                       GitHubUserDTO;
+  html_url:                    string;
+  description:                 null | string;
+  fork:                        boolean;
+  url:                         string;
+  languages_url:               string;
+  stargazers_url:              string;
+  contents_url:                string;
+  compare_url:                 string;
+  merges_url:                  string;
+  archive_url:                 string;
+  downloads_url:               string;
+  issues_url:                  string;
+  pulls_url:                   string;
+  milestones_url:              string;
+  notifications_url:           string;
+  labels_url:                  string;
+  releases_url:                string;
+  deployments_url:             string;
+  git_url:                     string;
+  stargazers_count:            number;
+  watchers_count:              number;
+  language:                    string | null;
+}
---
+++ src/app/models/github-repo-languages-dto.interface.ts
@@ -0,0 +1,3 @@
+export interface GitHubRepoLanguagesDTO {
+  [key: string]: number;
+}
